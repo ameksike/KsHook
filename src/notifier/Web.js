a9d3af330ccc58a30088195cca8f455af858d808
amembrides <antonio.membrides@sita.aero>
2023-09-07 15:19:39 +0200

    add web notifier

---
+++ src/notifier/Web.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+
+class Web {
+
+    constructor(cfg) {
+        this.cfg = {
+            url: process.env.CHANNEL_URL,
+            method: "post"
+        };
+        this.configure(cfg);
+    }
+
+    configure(cfg) {
+        cfg && Object.assign(this.cfg, cfg);
+        this.drv.configure(this.cfg);
+        return this;
+    }
+
+    /**
+     * @description execute HTTP notifications for Web Hooks
+     * @param {Object} payload 
+     * @param {Object} payload.target
+     * @param {Object|String} payload.target.value 
+     * @param {Object|String} payload.target.value.url
+     * @param {Object|String} payload.target.value.method
+     * @param {Object|String} payload.target.value.headers
+     * @returns {Object} Promise <{data: Object, status: Number, headers: Object, request: ClientRequest|XMLHttpRequest, config: Object }>
+     */
+    run(payload) {
+        const url = payload?.target?.value?.url || payload?.target?.value || this.cfg.url;
+        const headers = payload?.target?.value?.headers;
+        const method = payload?.target?.value?.method || this.cfg.method;
+        const req = { url, method };
+        if (headers) {
+            req.headers = headers;
+        }
+        if (payload?.data) {
+            if (method === "post") {
+                req.data = payload?.data;
+            } else {
+                req.params = payload?.data;
+            }
+        }
+        return axios(req);
+    }
+}
+
+module.exports = Web;
